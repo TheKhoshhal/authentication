e0524ff7cf5e4682a966b0c6d0692da1a84855db
TheKhoshhal <thekhoshhal@Gmail.com>
2024-08-14 18:00:07 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"auth-test/models/auth"
+	"auth-test/models/controllers"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	
+	e.GET("/", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "Accessible")
+	})
+
+	e.GET("/user/signin", controllers.SignInForm()).Name = "userSignInForm"
+	e.POST("/user/signin", controllers.SignIn())
+
+	adminGroup := e.Group("/admin")
+	adminGroup.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		Claims:                  &auth.Claims{},
+		SigningKey:              []byte(auth.GetJWTSecret()),
+		TokenLookup:             "cookie:access-token",
+		ErrorHandlerWithContext: auth.JWTErrorChecker,
+	}))
+
+	// Attach jwt token refresher.
+	adminGroup.Use(auth.TokenRefresherMiddleware)
+
+
+	adminGroup.GET("", controllers.Admin())
+
+	e.Logger.Fatal(e.Start(":8777"))
+}
+
